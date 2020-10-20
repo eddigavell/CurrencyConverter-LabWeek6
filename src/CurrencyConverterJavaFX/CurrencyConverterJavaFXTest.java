6822937c3c626df12989a80a56ce0205fed34037
Edvin Gavell <eddi@live.se>
2020-10-20 17:10:35 +0200

    removed empty

---
+++ src/CurrencyConverterJavaFX/CurrencyConverterJavaFXTest.java
@@ -0,0 +1,20 @@
+package CurrencyConverterJavaFX;
+
+import org.junit.jupiter.api.Test;
+
+public class CurrencyConverterJavaFXTest {
+
+    @Test
+    void TestInputsFromTextFieldToConvert() {
+        CurrencyConverterJavaFX whatToTest = new CurrencyConverterJavaFX();
+
+
+
+
+        //assertTrue(whatToTest.checkWhatComesFromTextFieldToConvert("100"));
+        //assertFalse(whatToTest.checkWhatComesFromTextFieldToConvert("kalle"));
+
+    }
+
+
+}
--- src/CurrencyConverterJavaFX/Main.java
+++
@@ -1,7 +0,0 @@
-package CurrencyConverterJavaFX;
-
-public class Main {
-    public static void main(String[] args) {
-        CurrencyConverterJavaFX.main(args);
-    }
-}
